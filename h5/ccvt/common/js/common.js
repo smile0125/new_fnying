2ed9747c758ccf792d0f24f45f39c62e9107f860
Edwin <1045416962@qq.com>
2019-01-11 11:35:27 +0800

    1

--- h5/ccvt/common/js/common.js
+++ h5/ccvt/common/js/common.js
@@ -22,3 +22,44 @@ function GetQueryString(name) {
     if (r != null) return unescape(r[2]);
     return null;
 }
+
+// 调用API共通函数
+function CallApi(api_url, post_data, suc_func, error_func) {
+
+    var api_site = 'http://www.fnying.com/php/';
+
+    post_data = post_data || {};
+    suc_func = suc_func || function () {};
+    error_func = error_func || function () {};
+
+    //console.log('Call API:' + api_url);
+    //console.log(JSON.stringify(post_data));
+
+    $.ajax({
+        url: api_site + api_url,
+        dataType: "jsonp",
+        data: post_data,
+        success: function (response) {
+            //console.log(JSON.stringify(response));
+            // API返回失败
+            if (response.errcode != 0) {
+                error_func(response);
+            } else {
+                // 成功处理数据
+                suc_func(response);
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            // API错误异常
+            var response = {"errcode": -1, "errmsg": '系统异常，请稍候再试'};
+            // 异常处理
+            error_func(response);
+        }
+    });
+}
+
+// 获取用户UUID
+function GetUUID(post_data, suc_func, error_func) {
+    var api_url = 'get_uuid.php';
+    CallApi(api_url, post_data, suc_func, error_func);
+}
